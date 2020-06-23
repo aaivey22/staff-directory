052b63de1dd2660fb680b0847205de7e9200c441
aaivey22 <angelawings22@gmail.com>
2020-06-23 16:20:20 -0400

    added src pages, initial components, and building the index page

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <span>Copyright Staff Directory 2020 by aaivey22@github.com</span>
+    </footer>
+  );
+}
+
+export default Footer;
