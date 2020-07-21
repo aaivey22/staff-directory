6f0d32397617d8237aa2678f1d6beac1c29ba20b
aaivey22 <angelawings22@gmail.com>
2020-07-21 18:45:04 -0400

    Updates

---
+++ precache-manifest.c7a8ff602da142e8e7bb29795426d932.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70f48ef3aa720c1e8953534acd3f01ff",
+    "url": "/staff-directory/index.html"
+  },
+  {
+    "revision": "0cb75c11e34b562faec3",
+    "url": "/staff-directory/static/css/main.c9db6add.chunk.css"
+  },
+  {
+    "revision": "b3525e2aab45f4813862",
+    "url": "/staff-directory/static/js/2.e9519c99.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/staff-directory/static/js/2.e9519c99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cb75c11e34b562faec3",
+    "url": "/staff-directory/static/js/main.6b7d484a.chunk.js"
+  },
+  {
+    "revision": "a2e333993de745f8f15c",
+    "url": "/staff-directory/static/js/runtime-main.c28052da.js"
+  }
+]);
